c10b3d7c62169ce9dc3bb6279bd23c8c43a678b2
bhawkeswood <bhawkeswood@gmail.com>
2013-10-02 18:32:09 -0400

    Writing tests for login and logout

--- spec/features/user_authentication_flows_spec.rb
+++ spec/features/user_authentication_flows_spec.rb
@@ -27,20 +27,58 @@ describe "User Authentication", js: true do
      within(:css, '.navbar') do
      	has_no_link?('Sign Up')
      	has_link?('Logout')
-     end
-  end
-
-  it "should fail registration" do
-  	visit "/users/new"
-  	user = FactoryGirl.build(:user)
-
-  	fill_in "user[email]", :with => user.email
-  	click_button "Create Account"
-
-  	expect(current_path).to eq(users_path)
-
-  	expect(page).to have_no_content("Account created")
-
+       end
     end
+
+    it "should fail registration" do
+    	visit "/users/new"
+    	user = FactoryGirl.build(:user)
+
+    	fill_in "user[email]", :with => user.email
+    	click_button "Create Account"
+
+    	expect(current_path).to eq(users_path)
+
+    	expect(page).to have_no_content("Account created")
+
+      within(:css, ".alert") do
+        have_content("Try again")
+        end
+      end
+
+    it "should successfully log in" do
+      visit '/'
+      find('.navbar').has_no_link?('Logout').should be_true
+      user = setup_signed_in_user
+      find('.navbar').has_link?('Logout').should be_true
+    end
+
+    it "should unsuccessfully log in" do
+      visit '/sessions/new'
+
+      fill_in "email", with: "a@b.com"
+      fill_in "password", with: "invalid creds"
+      click_button "Login"
+
+      expect(current_path).to eq(sessions_path)
+
+      page.should have_content('Invalid')
+      expect(page).to have_content('Invalid')
+    end
+
+    it "should successfully logout" do
+      user = setup_signed_in_user
+
+      visit '/'
+
+      find('.navbar').click_link 'Logout'
+
+      page.should have_content("Bye")
+      expect(page).to have_content("Bye")
+
+      find('.navbar').has_no_link?('Logout')
+    end
+
+
   end
 end
