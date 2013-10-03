52b1a2f200701fc9314cb1ca71e1f8528de19bc8
bhawkeswood <bhawkeswood@gmail.com>
2013-10-03 10:11:57 -0400

    Writing tests for pledge model

--- spec/models/pledge_spec.rb
+++ spec/models/pledge_spec.rb
@@ -1,5 +1,57 @@
 require 'spec_helper'
 
 describe Pledge do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it "should require a user" do
+		pledge = FactoryGirl.build(:pledge, user: nil)
+
+		pledge.should_not == pledge.save
+
+		pledge.should have(1).errors_on(:user_id)
+		expect(pledge).to have(1).errors_on(:user_id)
+	end
+
+	it "should require a project" do
+		pledge = FactoryGirl.build(:pledge, project: nil)
+
+		pledge.should_not == pledge.save
+
+		pledge.should have(1).errors_on(:project_id)
+		expect(pledge).to have(1).errors_on(:project_id)
+	end 
+
+	it "should require an amount" do
+		pledge = FactoryGirl.build(:pledge, amount: nil)
+
+		pledge.should_not == pledge.save
+
+		pledge.should have(1).errors_on(:amount)
+		expect(pledge).to have(1).errors_on(:amount)
+	end
+
+	it "should require an amount greater than 0" do
+		pledge = FactoryGirl.build(:pledge, amount: -10)
+
+		pledge.should_not ==pledge.save
+
+		pledge.should have(1).errors_on(:amount)
+		expect(pledge).to have(1).errors_on(:amount)
+	end
+
+	it "should require an amount to be an integer" do
+		pledge = FactoryGirl.build(:pledge, amount: 'abc')
+
+		pledge.should_not == pledge.save
+
+		pledge.should have(1).errors_on(:amount)
+		expect(pledge).to have(1).errors_on(:pledge)
+	end
+
 end
+
+
+
+
+
+
+
+
